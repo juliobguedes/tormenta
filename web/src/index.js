9211bf82cdc7d73a8c0942bcd5cbea58a221b1fe
Júlio Barreto <julio.costa@ccc.ufcg.edu.br>
2020-12-02 16:03:54 -0300

    testando basename

--- web/src/index.js
+++ web/src/index.js
@@ -1,5 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './components/app/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <BrowserRouter basename="/tormenta">
+        <App />
+    </BrowserRouter>,
+    document.getElementById('root')
+);
