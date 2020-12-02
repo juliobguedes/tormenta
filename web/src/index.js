d6d947f2330b67dd4d926be3cc86689ed3c0b3b2
Júlio Barreto <julio.costa@ccc.ufcg.edu.br>
2020-12-02 16:05:28 -0300

    v1

--- web/src/index.js
+++ web/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './components/app/App';
 
+console.log('V1');
+
 ReactDOM.render(
     <BrowserRouter basename="/tormenta">
         <App />
